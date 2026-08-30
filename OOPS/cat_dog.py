8b40f8b8b279f4ae38befb095b9e8031897d6106
mohd ahtasham ansari <aahtasham102@gmail.com>
2026-08-30 11:55:11 +0530

    feat: implement animal class hierarchy and demonstrate polymorphism with dog and cat subclasses

---
+++ OOPS/cat_dog.py
@@ -0,0 +1,17 @@
+from inherit import Animal,Dog,Cat
+
+
+def animal_sound(animals):
+    for animal in animals:
+        print(animal.name , end=" ")
+        if isinstance(animal,Dog):
+            print(animal.bark())
+        elif isinstance(animal,Cat):
+            print(animal.meow())
+
+
+dogs = [Dog("spiky", "german shepard"), Dog("buddy", "golden retriever")]
+cats = [Cat("whiskers", "persian"), Cat("lucy", "siamese")]
+
+animals = dogs + cats
+animal_sound(animals)
