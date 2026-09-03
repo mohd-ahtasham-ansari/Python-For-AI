34b005bf461faea2d271ab46a2fa91f07c423f54
mohd ahtasham ansari <aahtasham102@gmail.com>
2026-09-03 10:28:02 +0530

    feat: add examples for Python one-liners, comprehensions, and lambda functions

--- Advance_Python/2_oneLiner_&_comprehensens.py
+++ Advance_Python/2_oneLiner_&_comprehensens.py
@@ -6,21 +6,21 @@ if a % 2 == 0:
 else:
     print("odd number")
  """
-# a=230
+a=230
 
-# print("even number") if a % 2 ==0 else print("odd number") # ternary oprators
+print("even number") if a % 2 ==0 else print("odd number") # ternary oprators
 
 """ COMPREHENSIONS """
 
-# a=[1,2,3,4,5,6,7,8,9,10]
-# b=[]
+a=[1,2,3,4,5,6,7,8,9,10]
+b=[]
 
-# for i in a:
-#     if i%2==0:
-#         b.append(i)   
+for i in a:
+    if i%2==0:
+        b.append(i)   
 
-# print(a)
-# print(b)
+print(a)
+print(b)
 """  by  using comprehensions we can shorten it"""
 
 a=[1,2,3,4,5,6,7,8,9,10]
